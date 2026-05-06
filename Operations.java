59c2d57dfc488aa691d6a220e30adffe65e310fc
osppo <130026726+osppo@users.noreply.github.com>
2026-05-06 10:53:02 +0300

    features
    made the add and subtract method

--- Operations.java
+++ Operations.java
@@ -1,2 +1,14 @@
-public class Operations {
+class Operations {
+
+    public static void main(String[] args) {
+        System.out.println("add 2+3 : " + add(2,3));
+        System.out.println("subtract 3-2 : " + subtract(3,2));
+    }
+
+    public static int add(int num1,int num2){
+        return num1+num2;
+    }
+    public static int subtract(int num1,int num2){
+        return num1-num2;
+    }
 }
