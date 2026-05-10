d828c166552e9acee8dfe8a94dc435d34f71604c
osppo <130026726+osppo@users.noreply.github.com>
2026-05-10 21:19:10 +0300

    merged Rakan's work

--- Operations.java
+++ Operations.java
@@ -35,3 +35,21 @@ class Operations {
         return num1 + num2;
     }
 }
+public class Main {
+
+    public static int randomNumber() {
+        Random rand = new Random();
+
+        int num1 = rand.nextInt(10) + 1;
+        int num2 = rand.nextInt(20) + 1;
+
+        int result = num1 + num2;
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int answer = randomNumber();
+        System.out.println("Random result: " + answer);
+    }
+}
