3184e682b6eaf668c0f1ce0a237c552de4892caf
rmktt <rayan9090op@gmail.com>
2026-05-10 16:53:01 +0300

    Add average, percentage, and randomNumber functions to Operations class

--- Operations.java
+++ Operations.java
@@ -1,32 +1,37 @@
+import java.util.Random;
+
 class Operations {
 
     public static void main(String[] args) {
         System.out.println("add 2+3 : " + add(2,3));
         System.out.println("subtract 3-2 : " + subtract(3,2));
+        System.out.println("average of 4 and 8 : " + average(4,8));
+        System.out.println("percentage of 3 out of 12 : " + smallerToLargerPercentage(3,12) + "%");
+        System.out.println("Random result: " + randomNumber());
     }
 
     public static int add(int num1,int num2){
         return num1+num2;
     }
+
     public static int subtract(int num1,int num2){
         return num1-num2;
     }
-}
- class Random {
 
-    public static int randomNumber() {
-        Random rand = new Random();
-
-        int num1 = rand.nextInt(10) + 1;  
-        int num2 = rand.nextInt(20) + 1;   
-
-        int result = num1 + num2;
-
-        return result;
+    public static double average(double num1, double num2){
+        return (num1 + num2) / 2;
     }
 
-    public static void main(String[] args) {
-        int answer = randomNumber();
-        System.out.println("Random result: " + answer);
+    public static double smallerToLargerPercentage(double num1, double num2){
+        double smaller = Math.min(num1, num2);
+        double larger = Math.max(num1, num2);
+        return (smaller / larger) * 100;
+    }
+
+    public static int randomNumber(){
+        Random rand = new Random();
+        int num1 = rand.nextInt(10) + 1;
+        int num2 = rand.nextInt(20) + 1;
+        return num1 + num2;
     }
 }
